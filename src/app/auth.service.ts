cef8a1df6a0950333b0ec62c7bb4dbb2ce3d51bb
Alejandro <alex>
2019-01-21 13:49:36 +0100

    Añadido metodo en auth service para registrar un nuevo usuario

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  jwt: string = localStorage.getItem('jwt');
+
+  constructor(private http: HttpClient,private router:Router) { }
+
+  register(username: string, password: string) {
+    const body = { username, password };
+    return this.http.post('https://apitrello.herokuapp.com/users', body).toPromise();
+  }
+
 }
