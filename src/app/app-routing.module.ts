b4fdc02ebd1e797b9aed78a0287dc356697bbe60
Alejandro <alex>
2019-01-21 13:35:58 +0100

    Creadas rutas en app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BoardComponent } from './board/board.component';
+import { LoginViewComponent } from './login-view/login-view.component';
+import { RegisterViewComponent } from './register-view/register-view.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: 'board',
+    component: BoardComponent,
+  },
+  {
+    path: 'login',
+    component: LoginViewComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterViewComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
