2701f54ae652335b3d20492eac791cb4acbf70d9
Alejandro <alex>
2019-01-21 13:24:52 +0100

    Creados coponentes add-new-list,board,list,login-view,register-view , show-lits y taks

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddNewListComponent } from './add-new-list/add-new-list.component';
+import { BoardComponent } from './board/board.component';
+import { ListComponent } from './list/list.component';
+import { LoginViewComponent } from './login-view/login-view.component';
+import { RegisterViewComponent } from './register-view/register-view.component';
+import { ShowListsComponent } from './show-lists/show-lists.component';
+import { TaskComponent } from './task/task.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddNewListComponent,
+    BoardComponent,
+    ListComponent,
+    LoginViewComponent,
+    RegisterViewComponent,
+    ShowListsComponent,
+    TaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
