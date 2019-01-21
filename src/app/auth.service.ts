0952317f26b88feba5e65ef08812a61837257c47
Alejandro <alex>
2019-01-21 18:27:42 +0100

    Creado metodo para hacer login con un usuario y creados alertas para avisas al usuario de los posibles errores con el login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -11,6 +11,31 @@ export class AuthService {
 
   constructor(private http: HttpClient,private router:Router) { }
 
+  loginUser(username, password){
+    const body = { username, password };
+
+    return new Promise((resolve, reject) => {
+      this.http
+        .post('https://apitrello.herokuapp.com/users/login', body)
+        .toPromise()
+        .then(() => {
+          reject('User not found');
+        })
+        .catch(maybeNotAndError => {
+          if (maybeNotAndError.status === 200) {
+            const jwt = maybeNotAndError.error.text;
+            this.jwt = jwt;
+            localStorage.setItem('jwt', jwt);
+            resolve(200);
+          } else if (maybeNotAndError.status === 401) {
+            reject('Wrong password');
+          } else {
+            reject('Try again');
+          }
+        });
+    });
+  }
+
   registerUser(username: string, password: string) {
     const body = { username, password };
     return this.http.post('https://apitrello.herokuapp.com/users', body).toPromise();
--- src/app/login-view/login-view.component.ts
+++ src/app/login-view/login-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-view',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-view.component.scss']
 })
 export class LoginViewComponent implements OnInit {
+  username: string;
+  password: string;
+  errorLogin : boolean;
+  error: any;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
+  }
+
+  loginUser(){  
+    const { username, password } = this;
+    if (username !== undefined && password !== undefined) {
+      this.authService
+        .loginUser(username.trim(), password.trim())
+        .then(() => {
+          this.error = undefined;
+          this.router.navigate(['/board']);
+        })
+        .catch(error => {
+           this.errorLogin = true;
+          this.error = error;
+        });
+    }
   }
 
 }
