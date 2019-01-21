7bcc4e841db47ce6f1e49616c3c015b6c85f4e26
Alejandro <alex>
2019-01-21 18:10:25 +0100

    Creados alerts para avisar al usuario si el registro se ha completado correctamente o no

--- src/app/register-view/register-view.component.ts
+++ src/app/register-view/register-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -6,27 +6,37 @@ import { AuthService } from '../auth.service';
   templateUrl: './register-view.component.html',
   styleUrls: ['./register-view.component.scss']
 })
-export class RegisterViewComponent {
+export class RegisterViewComponent implements OnInit{
   username: string;
   password: string;
+  validForm : boolean = true;
+  userCreated : boolean = false;
   error: any;
   valid: any;
 
   constructor(private authService: AuthService) { }
 
+  ngOnInit() {
+    
+  }
 
   registerNewUser(){
     console.log("hola");
     const { username, password } = this;
-    if (username.trim() !== '' && password.trim() !== '') {
+    if (username !== undefined && password !== undefined) {
       this.authService
         .registerUser(username.trim(), password.trim())
         .then(res => {
+          console.log("quisme");
+          this.validForm = true;
+          this.userCreated = true;
           this.valid = res;
         })
         .catch(error => {
           this.error = error;
         });
+    }else{
+      this.validForm = false;
     }
   }
 }
